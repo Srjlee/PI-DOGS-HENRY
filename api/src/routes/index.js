2e0a2c0b56a5591175dad5ead354d7487c0076e0
Pablo Di Pietro <pablod.dipietro@gmail.com>
2022-04-21 00:37:36 -0300

    21-4-22

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,22 +1,13 @@
 require('dotenv').config();
-const { json } = require('body-parser');
 const { Router } = require('express');
-const axios = require('axios')
-const { Dog, Temperamento } = require('../db');
-const { Op } = require("sequelize");
 // Importar todos los routers;
 const temperamentRoutes = require('./temperament.js');
 const dogsRoutes = require('./dogs.js');
 const dogR = require('./dog.js');
 const router = Router();
-
 // Configurar los routers
 router.use('/temperament', temperamentRoutes);
 router.use('/dogs', dogsRoutes);
 router.use('/dog', dogR);
-
-
-
-
 
 module.exports = router;
--- client/src/components/Dogs.jsx
+++ client/src/components/Dogs.jsx
@@ -1,26 +1,41 @@
-import React,{useEffect} from 'react';
+import React,{useEffect, useState} from 'react';
 import Dog from './Dog';
+import Pagination from './Pagination';
 import { getAlldogs } from '../redux/actions/actions';
 import {useDispatch, useSelector} from 'react-redux';
 import './Dog.css'
 
-
 export default function Dogs() {
-  // if(!dogs) return <h2>No hay perros cargados</h2>
   const dispatch =useDispatch()
-
+  const [currentPage, setCurrentPage] = useState(1);
+  const dogsPerPage = 8;  
   useEffect(() => {
     dispatch(getAlldogs())
   }, [])
-
   const dogs = useSelector(state =>state)  
+
+  // Traigo los perros de la paginate
+  const indexPostAnterior = currentPage * dogsPerPage;
+  const indexPrimerPost = indexPostAnterior - dogsPerPage;
+  const currentDogs = !dogs.searchDog ? dogs.allDogs?.slice(indexPrimerPost, indexPostAnterior) : 
+  dogs.searchDog?.slice(indexPrimerPost, indexPostAnterior)
+
+  
+
+
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber)
+
     return (
     <div>
         <h1>Perros</h1>
     <div className="perros">
 
        {
-        !dogs.searchDog ? !dogs.allDogs ? <img src="https://i2.wp.com/revista.weepec.com/wp-content/uploads/2017/04/caminar.gif?zoom=1.5&resize=500%2C317&ssl=1" alt="Cargando" /> :  dogs.allDogs?.map(p=> (
+        !dogs.searchDog ? !dogs.allDogs ? <img src="https://i2.wp.com/revista.weepec.com/wp-content/uploads/2017/04/caminar.gif?zoom=1.5&resize=500%2C317&ssl=1" alt="Cargando" /> :  
+        <>
+        <div className="perros">
+        {currentDogs?.map(p=> (
            <Dog 
            key={p.id}
            image={p.image}
@@ -28,20 +43,38 @@ export default function Dogs() {
            temperament= {p.temperament}
            weight= {p.weight}
            id= {p.id}
-
            />
-         )) : 
-         dogs.searchDog.length === 0 ? <p>No se encontraron Resultados</p> :
-         dogs.searchDog?.map(p=> (
-          <Dog 
-          key={p.id}
-          image={p.image}
-          name= {p.name}
-          temperament= {p.temperament}
-          weight= {p.weight}
-          id= {p.id}
+         )) }
+          </div>        
 
-          />))
+         <div className="botonera">
+           <button > ◀️ </button>
+         <Pagination 
+         dogsPerPage={dogsPerPage} 
+         totalDogs={dogs.allDogs.length} 
+         paginate= {paginate}/>
+        <button> &gt; </button>
+         </div>
+         </>         
+         : 
+         dogs.searchDog.length === 0 ? <p>No se encontraron Resultados</p> :
+          <>
+          {currentDogs?.map(p=> (
+           <Dog 
+           key={p.id}
+           image={p.image}
+           name= {p.name}
+           temperament= {p.temperament}
+           weight= {p.weight}
+           id= {p.id}
+           /> ))}
+
+      <Pagination 
+      dogsPerPage={dogsPerPage} 
+      totalDogs={dogs.searchDog.length}
+      paginate= {paginate}/>
+          
+          </>
          
        }
     </div>
